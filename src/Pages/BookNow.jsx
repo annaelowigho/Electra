66012b3326e906df9d966328b942d406608b8e8d
annabelowigho <113777360+annaelowigho@users.noreply.github.com>
2025-05-26 21:22:27 +0100

    book now updated

--- src/Pages/BookNow.jsx
+++ src/Pages/BookNow.jsx
@@ -6,7 +6,7 @@ const BookNow = () => {
   return (
     <>
         <div className='py-[30px] md:py-[50px] px-[20px] md:px-[30px] xl:px-[100px]'>
-            <p className='text-[20px] md:text-[24px] font-medium'>
+            <p className='text-[20px] md:text-[24px] font-medium text-center'>
                 Please proceed to make a payment of ₦10,000 to book for the CNG conversion
             </p>
             <div className='py-[30px] md:py-[50px] px-[20px] md:px-[30px] xl:px-[200px]'>
@@ -22,52 +22,39 @@ const BookNow = () => {
                     <form action="">
                         <div className='space-y-4'>
                             <div className='flex flex-col gap-2 w-full'>
-                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Vehicle Type</label>
-                                <div className='rounded-md p-3 font-normal border-1'>
-                                    <select name="" id="" className='w-full outline-none'>
-                                        <option value="">Select Your Vehicle Type</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2 w-full'>
-                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Vehicle Specification</label>
-                                <div className='rounded-md p-3 font-normal border-1'>
-                                    <select name="" id="" className='w-full outline-none'>
-                                        <option value="">Select Your Vehicle Type</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2 w-full'>
-                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Service Date</label>
-                                <div className='rounded-md p-3 font-normal border-1'>
-                                    <input type="date" className='w-full outline-none'/>
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2 w-full'>
-                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Service Time</label>
-                                <div className='rounded-md p-3 font-normal border-1'>
-                                    <input type="time" className='w-full outline-none'/>
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2 w-full'>
-                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Phone Number</label>
-                                <div className='rounded-md p-3 font-normal border-1 flex justify-between items-center'>
-                                    <input type="text" className='w-full outline-none' placeholder='+234 800 000 0000'/>
-                                    <LuPhoneForwarded />
-                                </div>
+                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Full Name</label>
+                                <input type="text" placeholder='John Doe' 
+                                className='w-full rounded-md p-3 font-normal border-1 border-black/50'/>
                             </div>
                             <div className='flex flex-col gap-2 w-full'>
                                 <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Email Address</label>
-                                <div className='rounded-md p-3 font-normal border-1 flex justify-between items-center'>
-                                    <input type="text" className='w-full outline-none' placeholder='+234 800 000 0000'/>
-                                    <BiEnvelope />
-                                </div>
+                                <input type="email" placeholder='youremail@example.com' 
+                                className='w-full rounded-md p-3 font-normal border-1 border-black/50'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full'>
+                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Phone Number</label>
+                                <input type="email" placeholder='+2340123456789' 
+                                className='w-full rounded-md p-3 font-normal border-1 border-black/50'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full'>
+                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Vehicle Reg No</label>
+                                <input type="email" placeholder='GGE 123 ZY' 
+                                className='w-full rounded-md p-3 font-normal border-1 border-black/50'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full'>
+                                <label htmlFor="" className='text-[18px] md:text-[24px] font-semibold '>Vehicle Brand</label>
+                                <input type="email" placeholder='Toyota' 
+                                className='w-full rounded-md p-3 font-normal border-1 border-black/50'/>
                             </div>
                         </div>
+                        <div className='flex items-center pt-4 gap-3'>
+                            <span className='w-[20px] h-[20px] border-1 border-black/50 rounded-md'></span>
+                            <p> I agree to the Company's Terms and Conditions.</p>
+                        </div>
                         <button className='text-[18px] text-white font-semibold bg-[#002748] w-full rounded-lg cursor-pointer px-6 py-4 text-center
-                        mt-[15px] hover:shadow-lg transition-transform duration-500 hover:scale-102 hover:bg-[#2FEAE4] hover:border-[3px]
+                        mt-[15px] hover:shadow-lg transition-transform duration-500 hover:scale-101 hover:bg-[#2FEAE4] hover:border-[3px]
                         hover:border-[#002748] hover:text-[#002748]'>
-                            Open Application Form
+                            Book Now
                         </button>
                     </form>
                 </div>
